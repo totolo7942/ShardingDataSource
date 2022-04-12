edb31fd8d78012523a969da0ca200c2c7d301f53
백태성(Taesung Baek)/쿠폰/정산개발팀/11ST <aaron.baek@sk.com>
2022-04-12 17:55:21 +0900

    init

---
+++ src/test/java/com/example/shardingdatasource/ShardingDataSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shardingdatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingDataSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
